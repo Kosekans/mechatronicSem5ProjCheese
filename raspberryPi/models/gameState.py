c2184735d2e857d23231867f9528807ed7ba0c84
Kosekans <lkruyssee@gmail.com>
2024-12-28 22:16:59 +0100

    remove mode from string fro arduino, arduino doesnt have to know mode

--- raspberryPi/models/gameState.py
+++ raspberryPi/models/gameState.py
@@ -49,8 +49,7 @@ class GameState:
     
     def getInfoForAntrieb(self):
         return (
-            "{}/{}/{}/{}/{}/{}/{}/{}/{}".format(
-            self.chaesAntriebModus,
+            "{}/{}/{}/{}/{}/{}/{}/{}".format(
             self.goalCoordsVelo,
             self.gameMode,
             int(self.inverseSticks),
